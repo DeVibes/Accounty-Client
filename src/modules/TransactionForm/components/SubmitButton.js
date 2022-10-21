8b820c9b0e6f024bef7fb2fbe5c8821b62663177
Mark <replay.mkp@gmail.com>
2022-10-21 17:00:44 +0300

    fix: submit button was not clickable sometimes

--- src/modules/TransactionForm/components/SubmitButton.js
+++ src/modules/TransactionForm/components/SubmitButton.js
@@ -10,7 +10,7 @@ export const SubmitButton = ({ state, isFormLoading }) => {
     else if (isSuccess)
         buttonJsx = <ConfirmIcon size={25}/>;
     else 
-        buttonJsx = <input disabled={isFormLoading} type="submit" value="Save" className='cursor-pointer'/>;
+        buttonJsx = <input disabled={isFormLoading} type="submit" value="Save" className='cursor-pointer w-full'/>;
     return (
         <span className={`w-full text-white p-2 rounded-lg flex justify-center 
             ${isLoading || isSuccess ? "bg-slate-400 cursor-not-allowed" : 
