03b94c2d56aba1fc68592e567149b28ac54aac7c
Mark <replay.mkp@gmail.com>
2022-12-22 09:58:27 +0200

    fix: prod and qa env vars

--- src/config/config.js
+++ src/config/config.js
@@ -4,16 +4,12 @@ export const Environment = {
     PROD: "prod"
 };
 const env = process.env.NODE_ENV;
-const apiRouteLocal = process.env.REACT_APP_API_LOCAL;
-const apiRouteQA = process.env.REACT_APP_API;
-const apiRoute = env === Environment.LOCAL ? apiRouteLocal : apiRouteQA
+const apiRoute = process.env.REACT_APP_API;
 const gAuthClientId = process.env.REACT_APP_GAUTH_CLIENT_ID;
 const googleAPI = process.env.REACT_APP_GOOGLE_API;
 
 export {
     env,
-    apiRouteLocal,
-    apiRouteQA,
     apiRoute,
     gAuthClientId,
     googleAPI
