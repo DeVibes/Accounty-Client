5fd2fed428a11534839ff6990291d911656971b8
Mark <replay.mkp@gmail.com>
2023-01-01 22:09:07 +0200

    fix: analytics data fix

--- src/modules/Analytics/services/analytics.service.js
+++ src/modules/Analytics/services/analytics.service.js
@@ -1,5 +1,5 @@
-import { DateTime } from "luxon";
 import { Category } from "../../../utils/categories";
+const { DateTime } = require("luxon");
 
 export const mapDataForRechartPie = data => 
     data.map(({ categoryName: name, total: value }) => { 
@@ -39,14 +39,20 @@ export const getTimeFrame = () => {
     let fromDate, toDate;
     //TODO 10 should be user's choice
     if (currentDay > userCycle) {
+        // current day is between userCycle (10) and 28 / 30 / 31
+        // fromDate should be 11 of the current month start day
+        // toDate should be 10 of the next month end day (increase the year if its December)
         fromDate = DateTime.local(currentYear, currentMonth, userCycle + 1).startOf('day').toUTC().toISO();   //'YYYY-MM-DDTHH:MM:SS.MSZ'
         toDate = DateTime.local(currentMonth === 12 ? currentYear + 1 : currentYear, 
             currentMonth === 12 ? 1 : currentMonth + 1, userCycle).endOf('day').toUTC().toISO();
     }
     else {
+        // current day is between 1 and userCycle (10)
+        // fromDate should be 11 of the previous month start day (subtract the year if its January)
+        // toDate should be 10 of the current month end day
         fromDate = DateTime.local(currentMonth === 1 ? currentYear - 1 : currentYear, 
-            currentMonth - 1, userCycle + 1).endOf('day').toUTC().toISO();
-        toDate = DateTime.local(currentYear, currentMonth, userCycle).startOf('day').toUTC().toISO();
+            currentMonth === 1 ? 12 : currentMonth - 1, userCycle + 1).startOf('day').toUTC().toISO();
+        toDate = DateTime.local(currentYear, currentMonth, userCycle).endOf('day').toUTC().toISO();
     }
     return { fromDate, toDate }
 };
